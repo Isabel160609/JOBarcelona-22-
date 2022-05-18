824399f61d145b7b405b7bef49a392c1d4ae74ce
Isabel Domenech <77856098+Isabel160609@users.noreply.github.com>
2022-05-18 20:29:22 +0200

    Add files via upload

---
+++ JobX/src/main/java/com/init/jobX/controller/UserListController.java
@@ -0,0 +1,34 @@
+package com.init.jobX.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.init.jobX.segurity.entity.User;
+import com.init.jobX.service.UserListService;
+
+@RestController
+@RequestMapping("/")
+public class UserListController {
+
+	@Autowired
+	UserListService userListService;
+	
+	@GetMapping(value = "/users")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<List<User>> getUsers() {
+		try {
+			List<User> users = userListService.listUsers();
+			return new ResponseEntity<List<User>>(users, HttpStatus.FOUND);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+	}
+}
---
+++ JobX/src/main/java/com/init/jobX/segurity/entity/User.java
@@ -0,0 +1,91 @@
+package com.init.jobX.segurity.entity;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+	@Id
+    @GeneratedValue(generator = "idGenerator")
+    @GenericGenerator(name = "idGenerator", strategy = "uuid")
+    private String id;
+
+ 
+    @Column(unique = true)
+    private String username;
+ 
+    private String email;
+    
+    private String password;
+    
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_rol", joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "rol_id"))
+    private Set<Rol> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User (String userName, String email, String password) {
+
+        this.username = userName;
+        this.email = email;
+        this.password = password;
+    }
+    
+    public User (String id,String userName, String email, String password,Set<Rol> roles) {
+        this.id=id;
+    
+        this.username = userName;
+        this.email = email;
+        this.password = password;
+        this.roles=roles;
+    }
+    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String UserName) {
+    	
+        this.username = UserName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Rol> roles) {
+        this.roles = roles;
+    }
+}
